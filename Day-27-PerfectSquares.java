98844d0fe4db3d2f5d706fa28ec35b5ef4b40095
Sagnik Chattopadhyaya <36898274+sagnik20@users.noreply.github.com>
2020-06-28 09:20:06 +0530

    Added Day-27-PerfectSquares.java

---
+++ Day-27-PerfectSquares.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numSquares(int n) {
+        if(n < 4) return n;
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0; dp[1] = 1; dp[2] = 2; dp[3] = 3;
+        
+        for(int i = 4; i <= n; i++) 
+            for(int j = 1; j * j <= i; j++)
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+        
+        return dp[n];
+    }
+}
